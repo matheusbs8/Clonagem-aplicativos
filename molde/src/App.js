3a7a3e2209fe3b2d55389aff0e76688346380fad
matheusbs8 <matheusbs@dcc.ufrj.br>
2023-02-02 19:57:18 -0300

    construindo modelo-padrao de aplicativo

---
+++ molde/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+  return(
+    <div>
+
+      <h1>Hello word!!</h1>
+      <p>Este é o meu primeiro aplicativo</p>
+    </div>
+    
+  )
+}
+
+export default App;
