7880b5f4aac62b8c2021478c5194e12d998aa4f7
matheusbs8 <matheusbs@dcc.ufrj.br>
2023-02-11 22:34:03 -0300

    liked publicacao

---
+++ Instagram/src/Utils/Publicacao.tsx
@@ -0,0 +1,195 @@
+const Publicacoes = [
+    {
+      label: "Cristina",
+      image: require('../../assets/imagens/Stories/Story-perfil (1).jpg'),
+      verificado:false,
+      saved:true,
+      descricao:`Obrigado, Nação, pelo apoio até o final. É um privilégio ter a Maior Torcida do Mundo do nosso lado. #VamosFlamengo 📸 @marcelocortes_ / CRF`,
+      imagemPubli:require('../../assets/imagens/Publicacao/publi1.jpg'),
+      Ncurtidas:20,
+      comentarios:[
+        {
+          nameOfPerfil:'Joao',
+          descricao:'Obrigado, Nação, pelo apoio até o final. É um privilégio ter a Maior Torcida do Mundo do nosso lado.',
+        },
+        {
+          nameOfPerfil:'Maria',
+          descricao:'Obrigado, Nação, pelo apoio até o final. É um privilégio ter a Maior Torcida do Mundo do nosso lado.',
+        },
+      ]
+    },
+    {
+      label: "Marcos",
+      image: require('../../assets/imagens/Stories/Story-perfil (4).jpg'),
+      verificado:false,
+      saved:true,
+      descricao:`Obrigado, Nação, pelo apoio até o final. É um privilégio ter a Maior Torcida do Mundo do nosso lado. #VamosFlamengo 📸 @marcelocortes_ / CRF`,
+      imagemPubli:require('../../assets/imagens/Publicacao/publi2.jpg'),
+      Ncurtidas:20,
+      comentarios:[
+        {
+          nameOfPerfil:'Joao',
+          descricao:'Obrigado, Nação, pelo apoio até o final. É um privilégio ter a Maior Torcida do Mundo do nosso lado.',
+        },
+        {
+          nameOfPerfil:'Maria',
+          descricao:'Obrigado, Nação, pelo apoio até o final. É um privilégio ter a Maior Torcida do Mundo do nosso lado.',
+        },
+      ]
+    },
+    {
+      label: "Alex",
+      image: require('../../assets/imagens/Stories/Story-perfil (2).jpg'),
+      verificado:true,
+      saved:true,
+      descricao:`Obrigado, Nação, pelo apoio até o final. É um privilégio ter a Maior Torcida do Mundo do nosso lado. #VamosFlamengo 📸 @marcelocortes_ / CRF`,
+      imagemPubli:require('../../assets/imagens/Publicacao/publi3.jpg'),
+      Ncurtidas:20,
+      comentarios:[
+        {
+          nameOfPerfil:'Joao',
+          descricao:'Obrigado, Nação, pelo apoio até o final. É um privilégio ter a Maior Torcida do Mundo do nosso lado.',
+        },
+        {
+          nameOfPerfil:'Maria',
+          descricao:'Obrigado, Nação, pelo apoio até o final. É um privilégio ter a Maior Torcida do Mundo do nosso lado.',
+        },
+      ]
+    },
+    {
+      label: "Maria",
+      image: require('../../assets/imagens/Stories/Story-perfil (3).jpg'),
+      verificado:false,
+      saved:true,
+      descricao:`Obrigado, Nação, pelo apoio até o final. É um privilégio ter a Maior Torcida do Mundo do nosso lado. #VamosFlamengo 📸 @marcelocortes_ / CRF`,
+      imagemPubli:require('../../assets/imagens/Publicacao/publi4.jpg'),
+      Ncurtidas:20,
+      comentarios:[
+        {
+          nameOfPerfil:'Joao',
+          descricao:'Obrigado, Nação, pelo apoio até o final. É um privilégio ter a Maior Torcida do Mundo do nosso lado.',
+        },
+        {
+          nameOfPerfil:'Maria',
+          descricao:'Obrigado, Nação, pelo apoio até o final. É um privilégio ter a Maior Torcida do Mundo do nosso lado.',
+        },
+      ]
+    },
+    {
+      label: "Roberta",
+      image: require('../../assets/imagens/Stories/Story-perfil (5).jpg'),
+      verificado:true,
+      saved:true,
+      descricao:`Obrigado, Nação, pelo apoio até o final. É um privilégio ter a Maior Torcida do Mundo do nosso lado. #VamosFlamengo 📸 @marcelocortes_ / CRF`,
+      imagemPubli:require('../../assets/imagens/Publicacao/publi5.jpg'),
+      Ncurtidas:20,
+      comentarios:[
+        {
+          nameOfPerfil:'Joao',
+          descricao:'Obrigado, Nação, pelo apoio até o final. É um privilégio ter a Maior Torcida do Mundo do nosso lado.',
+        },
+        {
+          nameOfPerfil:'Maria',
+          descricao:'Obrigado, Nação, pelo apoio até o final. É um privilégio ter a Maior Torcida do Mundo do nosso lado.',
+        },
+      ]
+    },
+    {
+      label: "Larissa",
+      image: require('../../assets/imagens/Stories/Story-perfil (6).jpg'),
+      verificado:false,
+      saved:true,
+      descricao:`Obrigado, Nação, pelo apoio até o final. É um privilégio ter a Maior Torcida do Mundo do nosso lado. #VamosFlamengo 📸 @marcelocortes_ / CRF`,
+      imagemPubli:require('../../assets/imagens/Publicacao/publi6.jpg'),
+      Ncurtidas:20,
+      comentarios:[
+        {
+          nameOfPerfil:'Joao',
+          descricao:'Obrigado, Nação, pelo apoio até o final. É um privilégio ter a Maior Torcida do Mundo do nosso lado.',
+        },
+        {
+          nameOfPerfil:'Maria',
+          descricao:'Obrigado, Nação, pelo apoio até o final. É um privilégio ter a Maior Torcida do Mundo do nosso lado.',
+        },
+      ]
+    },
+    {
+      label: "Camila",
+      image: require('../../assets/imagens/Stories/Story-perfil (7).jpg'),
+      verificado:false,
+      saved:true,
+      descricao:`Obrigado, Nação, pelo apoio até o final. É um privilégio ter a Maior Torcida do Mundo do nosso lado. #VamosFlamengo 📸 @marcelocortes_ / CRF`,
+      imagemPubli:require('../../assets/imagens/Publicacao/publi7.jpg'),
+      Ncurtidas:20,
+      comentarios:[
+        {
+          nameOfPerfil:'Joao',
+          descricao:'Obrigado, Nação, pelo apoio até o final. É um privilégio ter a Maior Torcida do Mundo do nosso lado.',
+        },
+        {
+          nameOfPerfil:'Maria',
+          descricao:'Obrigado, Nação, pelo apoio até o final. É um privilégio ter a Maior Torcida do Mundo do nosso lado.',
+        },
+      ]
+    },
+    {
+      label: "Carlos",
+      image: require('../../assets/imagens/Stories/Story-perfil (8).jpg'),
+      verificado:false,
+      saved:true,
+      descricao:`Obrigado, Nação, pelo apoio até o final. É um privilégio ter a Maior Torcida do Mundo do nosso lado. #VamosFlamengo 📸 @marcelocortes_ / CRF`,
+      imagemPubli:require('../../assets/imagens/Publicacao/publi8.jpg'),
+      Ncurtidas:20,
+      comentarios:[
+        {
+          nameOfPerfil:'Joao',
+          descricao:'Obrigado, Nação, pelo apoio até o final. É um privilégio ter a Maior Torcida do Mundo do nosso lado.',
+        },
+        {
+          nameOfPerfil:'Maria',
+          descricao:'Obrigado, Nação, pelo apoio até o final. É um privilégio ter a Maior Torcida do Mundo do nosso lado.',
+        },
+      ]
+    },
+    {
+      label: "Giovanna",
+      image: require('../../assets/imagens/Stories/Story-perfil (9).jpg'),
+      verificado:false,
+      saved:true,
+      descricao:`Obrigado, Nação, pelo apoio até o final. É um privilégio ter a Maior Torcida do Mundo do nosso lado. #VamosFlamengo 📸 @marcelocortes_ / CRF`,
+      imagemPubli:require('../../assets/imagens/Publicacao/publi9.jpg'),
+      Ncurtidas:20,
+      comentarios:[
+        {
+          nameOfPerfil:'Joao',
+          descricao:'Obrigado, Nação, pelo apoio até o final. É um privilégio ter a Maior Torcida do Mundo do nosso lado.',
+        },
+        {
+          nameOfPerfil:'Maria',
+          descricao:'Obrigado, Nação, pelo apoio até o final. É um privilégio ter a Maior Torcida do Mundo do nosso lado.',
+        },
+      ]
+    },
+    {
+      label: "Rafael",
+      image: require('../../assets/imagens/Stories/Story-perfil (10).jpg'),
+      verificado:false,
+      saved:true,
+      descricao:`Obrigado, Nação, pelo apoio até o final. É um privilégio ter a Maior Torcida do Mundo do nosso lado. #VamosFlamengo 📸 @marcelocortes_ / CRF`,
+      imagemPubli:require('../../assets/imagens/Publicacao/publi10.jpg'),
+      Ncurtidas:20,
+      comentarios:[
+        {
+          nameOfPerfil:'Joao',
+          descricao:'Obrigado, Nação, pelo apoio até o final. É um privilégio ter a Maior Torcida do Mundo do nosso lado.',
+        },
+        {
+          nameOfPerfil:'Maria',
+          descricao:'Obrigado, Nação, pelo apoio até o final. É um privilégio ter a Maior Torcida do Mundo do nosso lado.',
+        },
+      ]
+    },
+   
+  ];
+  
+  export default Publicacoes;
